a92f7e417730b8e628add308ff87ba310965d5dd
Javiera Parada E.- <jparada@digital.gob.cl>
2020-08-26 09:00:50 -0400

    añadiendo template

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,5 @@
 #Utils from django
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 #From Utils general
 from datetime import datetime
@@ -9,20 +7,29 @@ from datetime import datetime
 #Definiendo var global
 posts = [
     {
+        'titulo' : 'Andando1',
+        'usuario':  {
         'nombre': 'Pepito Falcon',
-        'usuario': 'Papito',
+        'avatar': '',
+        },
         'timestap': datetime.now().strftime('%b %d, %Y -%H:%M hrs'),
         'imagen_publicada': 'https://sites.google.com/site/thesimpsonsxdddddd/_/rsrc/1375821405412/homero/hommer.jpg?height=320&width=258',
     },
     {
-        'nombre': 'Juana Falcon',
-        'usuario': 'Papito',
+        'titulo' : 'Papito',
+        'usuario':  {
+        'nombre': 'Alex Falcon',
+        'avatar': '',
+        },
         'timestap': datetime.now().strftime('%b %d, %Y -%H:%M hrs'),
         'imagen_publicada': 'https://sites.google.com/site/thesimpsonsxdddddd/_/rsrc/1375821405412/homero/hommer.jpg?height=320&width=258',
     },
     {
-        'nombre': 'Ariel Falcon',
-        'usuario': 'Papito',
+        'titulo' : 'Bob',
+        'usuario':  {
+        'nombre': 'Patricio Falcon',
+        'avatar': '',
+        },
         'timestap': datetime.now().strftime('%b %d, %Y -%H:%M hrs'),
         'imagen_publicada': 'https://sites.google.com/site/thesimpsonsxdddddd/_/rsrc/1375821405412/homero/hommer.jpg?height=320&width=258',
     }
@@ -30,12 +37,4 @@ posts = [
 
 # Create your views here.
 def list_posts(request):
-    content = []
-    for post in posts:
-        content.append("""
-        <p><strong>{nombre}</strong></p>
-        <p><small>{usuario}-<i>{timestap}</i></small></p>
-        <figure><img src={imagen_publicada}/></strong>
-        """.format(**post))
-
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
